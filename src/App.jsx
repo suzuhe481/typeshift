1a425d9f8d4daceb65a3b705fa15cd6a37648f88
Hector <baseballhector@gmail.com>
2024-03-07 23:46:23 -0500

    Using components for title and game container

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Title from "./components/Title/Title";
+import GameContainer from "./components/GameContainer/GameContainer";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Title />
+      <GameContainer />
+    </>
+  );
 }
 
 export default App;

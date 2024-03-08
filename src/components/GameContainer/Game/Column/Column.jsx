6ab6ecf7069b61ac21544f657aefccbb0fbc78a2
Hector <baseballhector@gmail.com>
2024-03-07 23:58:16 -0500

    Column component

---
+++ src/components/GameContainer/Game/Column/Column.jsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp, faArrowDown } from "@fortawesome/free-solid-svg-icons";
+
+import "./Column.scss";
+
+function Column(props) {
+  const [letters, setLetters] = useState(props.letters);
+  // const letters = props.letters;
+
+  // Moves the words in the column up.
+  const moveUp = () => {
+    // Don't move letters if top letter is not empty.
+    if (letters[0] != " ") {
+      return;
+    }
+
+    // Moves the first element to the end.
+    // NOTE: Slice is used because react won't rerender the page without it.
+    // It doesn't see the change within the array as a change to the variable so it doesn't rerender it.
+    letters.push(letters.shift());
+    setLetters(letters.slice());
+
+    var newWord = [...props.viewedWord];
+    newWord[props.index] = letters[1];
+    newWord = newWord.join("");
+    // console.log(newWord);
+    props.setViewedWord(newWord);
+  };
+
+  // Moves the words in the column down.
+  const moveDown = () => {
+    // Don't move letters if bottom letter is not empty.
+    if (letters[letters.length - 1] != " ") {
+      return;
+    }
+
+    // Moves the last element to the front.
+    // NOTE: Slice is used because react won't rerender the page without it.
+    // It doesn't see the change within the array as a change to the variable so it doesn't rerender it.
+    letters.unshift(letters.pop());
+    setLetters(letters.slice());
+
+    var newWord = [...props.viewedWord];
+    newWord[props.index] = letters[1];
+    newWord = newWord.join("");
+    // console.log(newWord);
+    props.setViewedWord(newWord);
+  };
+
+  const lettersArray = Array.from(letters);
+
+  const cells = lettersArray.map((letter, index) => {
+    if (index === 1) {
+      return (
+        <div key={index} className="cell selection">
+          {letter}
+        </div>
+      );
+    } else {
+      return (
+        <div key={index} className="cell">
+          {letter}
+        </div>
+      );
+    }
+  });
+
+  return (
+    <div className="column">
+      <FontAwesomeIcon icon={faArrowUp} onClick={moveUp} className="arrow" />
+      {cells}
+      <FontAwesomeIcon
+        icon={faArrowDown}
+        onClick={moveDown}
+        className="arrow"
+      />
+    </div>
+  );
+}
+
+export default Column;

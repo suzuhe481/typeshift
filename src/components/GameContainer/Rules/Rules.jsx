cb561fa014a0d2f0f2750e08473bc0775d62e905
Hector <baseballhector@gmail.com>
2024-03-07 23:57:46 -0500

    Rules component

---
+++ src/components/GameContainer/Rules/Rules.jsx
@@ -0,0 +1,19 @@
+function Rules() {
+  return (
+    <div className="rules">
+      <p>Rules</p>
+      <br />
+      <p>This is where the rules and instructions for playing typeshift go.</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+        nesciunt veritatis, possimus quae qui fuga dolore quibusdam quisquam
+        excepturi nostrum, accusamus temporibus ratione minus, eligendi
+        architecto autem distinctio ut quos eius cumque voluptates eos numquam
+        vero amet! Libero odio atque error ex, recusandae, repudiandae nihil,
+        sunt deserunt ea neque praesentium!
+      </p>
+    </div>
+  );
+}
+
+export default Rules;

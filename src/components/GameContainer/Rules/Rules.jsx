a67c168b2800fbac706ba3283beb19b674619caa
Hector <baseballhector@gmail.com>
2024-03-08 15:37:00 -0500

    Removed unnecessary text

--- src/components/GameContainer/Rules/Rules.jsx
+++ src/components/GameContainer/Rules/Rules.jsx
@@ -3,7 +3,6 @@ function Rules() {
     <div className="rules">
       <p>Rules</p>
       <br />
-      <p>This is where the rules and instructions for playing typeshift go.</p>
       <p>
         In Typeshift, the goal is to find all the possible words with the given
         letters. Click/tap the arrows to move the letters in each columns.

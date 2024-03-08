0642ff5396cde2246827bdde5f158d00136d6a91
Hector <baseballhector@gmail.com>
2024-03-07 23:57:55 -0500

    Words list component

---
+++ src/components/GameContainer/WordsList/WordsList.jsx
@@ -0,0 +1,11 @@
+function WordsList() {
+  return (
+    <div className="words-list">
+      <p>WordsList</p>
+      <br />
+      <p>This is where the found words will appear.</p>
+    </div>
+  );
+}
+
+export default WordsList;

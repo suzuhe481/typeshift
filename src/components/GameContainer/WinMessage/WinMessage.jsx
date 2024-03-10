f12853b6c00a5201a0b44dbe5b4e4c45f0b0ae81
Hector <baseballhector@gmail.com>
2024-03-10 18:42:51 -0400

    Class name change

--- src/components/GameContainer/WinMessage/WinMessage.jsx
+++ src/components/GameContainer/WinMessage/WinMessage.jsx
@@ -4,7 +4,7 @@ function WinMessage(props) {
   const gameWon = props.gameWon;
 
   const message = (
-    <div className={`message ${gameWon && "show"}`}>You Win!</div>
+    <div className={`game__message ${gameWon && "show"}`}>You Win!</div>
   );
 
   return <>{message}</>;

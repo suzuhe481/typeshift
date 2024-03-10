775b2712360ab64faa10f9288ff06885257238c1
Hector <baseballhector@gmail.com>
2024-03-10 18:40:54 -0400

    Changes to class names

--- src/components/GameContainer/WordsList/WordsList.jsx
+++ src/components/GameContainer/WordsList/WordsList.jsx
@@ -4,13 +4,14 @@ function WordsList(props) {
   const foundWords = props.foundWords;
 
   const wordsList = foundWords.map((word, index) => (
-    <li key={index}>{word}</li>
+    <li key={index} className="words_list__word">
+      {word}
+    </li>
   ));
 
   return (
-    <div className="words-list">
-      <p>Found Words</p>
-      <br />
+    <div className="words_list">
+      <p className="words_list__title">Found Words</p>
       <ul>{wordsList}</ul>
     </div>
   );

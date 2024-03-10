f80d731a1fe0eb1d6bea29cc806e2794c58a7b69
Hector <baseballhector@gmail.com>
2024-03-10 18:44:01 -0400

    Class name changes

--- src/components/GameContainer/Rules/Rules.jsx
+++ src/components/GameContainer/Rules/Rules.jsx
@@ -3,9 +3,8 @@ import "./Rules.scss";
 function Rules() {
   return (
     <div className="rules">
-      <p>Rules</p>
-      <br />
-      <p>
+      <p className="rules__title">Rules</p>
+      <p className="rules__text">
         In Typeshift, the goal is to find all the possible words with the given
         letters. Click/tap the arrows to move the letters in each columns.
         <br />

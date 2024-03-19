9c3247a3d5783f89c252c857c42a1d7e3549582f
Hector <baseballhector@gmail.com>
2024-03-19 17:13:16 -0400

    Imported useRef

--- src/components/GameContainer/Game/Column/Column.jsx
+++ src/components/GameContainer/Game/Column/Column.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 import PropTypes from "prop-types";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";

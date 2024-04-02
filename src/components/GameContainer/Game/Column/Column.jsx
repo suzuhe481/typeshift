b60277e7106ef755c481f2d7c5ffd9661e415c55
Hector <baseballhector@gmail.com>
2024-04-01 20:44:47 -0400

    Refactored mouse down/up/drag.

--- src/components/GameContainer/Game/Column/Column.jsx
+++ src/components/GameContainer/Game/Column/Column.jsx
@@ -9,8 +9,6 @@ import "./Column.scss";
 const COLUMN_ANIMATION_TIME_S = 0.1; // In seconds
 const COLUMN_ANIMATION_TIME_MS = COLUMN_ANIMATION_TIME_S * 1000; // DON'T CHANGE. In milliseconds
 const COLUMN_TRANSITION_ANIMATION = `all ${COLUMN_ANIMATION_TIME_S}s ease-in-out`;
-const COLUMN_UPPER_LIMIT = 0; // In pixels
-const COLUMN_LOWER_LIMIT = 100; // In pixels
 
 const WORD_GROW_ANIMATION_S = 0.2; // In seconds
 const WORD_GROW_ANIMATION_MS = WORD_GROW_ANIMATION_S * 1000; // DON'T CHANGE. In milliseconds.
@@ -20,35 +18,46 @@ const BOX_HEIGHT = 100; // In pixels
 
 function Column({
   letters,
+  initialPosition,
   gameWon,
-  viewedWord,
-  setViewedWord,
-  sendViewedWord,
-  index,
+  columnIndex,
   wordIsFound,
   setWordIsFound,
   onTouchScreen,
+  currentWord,
+  setCurrentWord,
+  longestColumn,
 }) {
+  // The bounds of where the column can move.
+  // Lower - Prevents column from moving below this limit.
+  // Upper - Prevents column from mobing above this limit.
+  var COLUMN_LOWER_LIMIT = (longestColumn - letters.length) * -50; // In pixels
+  var COLUMN_UPPER_LIMIT =
+    COLUMN_LOWER_LIMIT - (letters.length - 1) * BOX_HEIGHT; // In pixels
+
   // State
   const [isAnimated, setIsAnimated] = useState(false);
-  const [colPosition, setColPosition] = useState(1);
+  const [colPosition, setColPosition] = useState(initialPosition);
+  const [isDragging, setIsDragging] = useState(false);
+
+  const startPosition =
+    colPosition * -100 + (longestColumn - letters.length) * -50;
 
   // Refs
   const columnRef = useRef(null);
   const initialYRef = useRef(0);
-  const draggedYRef = useRef(0);
+  const draggedYRef = useRef(startPosition);
   const arrowUpRef = useRef(null);
   const arrowDownRef = useRef(null);
   const currentLetterRef = useRef(null);
-
-  // Constant variables
-  const numOfBoxes = letters.length;
+  const columnTranslatePositionRef = useRef(startPosition);
 
   // Styles
   var columnStyle = {
     display: `flex`,
     flexDirection: `column`,
     width: `100px`,
+    position: `relative`,
   };
 
   var arrowStyle = {
@@ -57,100 +66,82 @@ function Column({
     color: `#5dbafc`,
   };
 
-  // Callbacks
-  // Handles mouse or finger drag event to animate column movement.
-  const handleDrag = useCallback(
-    (e) => {
-      if (onTouchScreen) {
-        const deltaY = initialYRef.current - e.touches[0].clientY;
-        initialYRef.current = e.touches[0].clientY;
-        draggedYRef.current -= deltaY;
-
-        columnRef.current.style.transform = `translate(0px, ${draggedYRef.current}px)`;
-      } else {
-        const deltaY = initialYRef.current - e.clientY;
-        initialYRef.current = e.clientY;
-        draggedYRef.current -= deltaY;
-
-        columnRef.current.style.transform = `translate(0px, ${draggedYRef.current}px)`;
-      }
-    },
-    [onTouchScreen]
-  );
-
-  // Handles removing event listeners for dragging and mouse/touchend events.
-  const handleMouseUp = useCallback(() => {
-    if (gameWon) {
-      return;
-    }
-
-    if (onTouchScreen) {
-      document.removeEventListener("touchmove", handleDrag);
-      document.removeEventListener("touchend", handleMouseUp);
-    } else {
-      document.removeEventListener("mousemove", handleDrag);
-      document.removeEventListener("mouseup", handleMouseUp);
-    }
-
-    normalizeColumnPosition();
-  }, [gameWon, handleDrag, onTouchScreen]);
-
-  const letterBoxes = letters.map((letter, index) => {
-    if (colPosition === index) {
+  // Creates the cells containing each letter.
+  const letterBoxes = letters.map((letter, i) => {
+    if (i === colPosition) {
       return (
-        <div key={index} ref={currentLetterRef} className="cell">
+        <div key={i} ref={currentLetterRef} className="cell current">
           {letter}
         </div>
       );
     } else {
       return (
-        <div key={index} className="cell">
+        <div key={i} className="cell">
           {letter}
         </div>
       );
     }
   });
 
-  // On initial page load.
-  // Adds the mousedown event listener to column.
-  useEffect(() => {
-    const handleMouseDown = (e) => {
+  // Stores the location of the initial click/tap.
+  const handleMouseDown = useCallback(
+    (e) => {
       // Prevents columns from being dragged if game is over.
       if (gameWon) {
         return;
       }
 
-      initialYRef.current = e.clientY;
+      setIsDragging(true);
 
+      // Stores the initial location where the user tapped or clicked.
       if (onTouchScreen) {
         initialYRef.current = e.touches[0].clientY;
-
-        document.addEventListener("touchmove", handleDrag);
-        document.addEventListener("touchend", handleMouseUp);
       } else {
         initialYRef.current = e.clientY;
-
-        document.addEventListener("mousemove", handleDrag);
-        document.addEventListener("mouseup", handleMouseUp);
       }
-    };
+    },
+    [gameWon, onTouchScreen]
+  );
 
-    const colRefVar = columnRef;
-    colRefVar.current.addEventListener("mousedown", handleMouseDown);
-    colRefVar.current.addEventListener("touchstart", handleMouseDown);
+  // Moves columns to appropriate positions.
+  // Calculates the new word.
+  const handleMouseUp = useCallback(() => {
+    if (!isDragging) {
+      return;
+    }
 
-    return () => {
-      colRefVar.current.removeEventListener("mousedown", handleMouseDown);
-      colRefVar.current.removeEventListener("touchstart", handleMouseDown);
-    };
-  }, [onTouchScreen, gameWon]);
+    setIsDragging(false);
+    resetColumnAfterMouseUp();
+    calculateColumnPosition();
+  }, [isDragging]);
 
-  // If column is outside of it's bounds, animate it so it goes back to an appropriate location.
-  // When column is dragged above its upper limit, lower it to the upper limit.
-  // A timer in useEffect removes then transition animation.
+  // Moves the column to the appropriate distance dragged.
+  const handleDrag = useCallback(
+    (e) => {
+      if (!isDragging) {
+        return;
+      }
+
+      // Calculates the difference between the initial location clicked and the distance dragged.
+      if (onTouchScreen) {
+        const deltaY = initialYRef.current - e.touches[0].clientY;
+        initialYRef.current = e.touches[0].clientY;
+        draggedYRef.current -= deltaY;
+      } else {
+        const deltaY = initialYRef.current - e.clientY;
+        initialYRef.current = e.clientY;
+        draggedYRef.current -= deltaY;
+      }
+      columnRef.current.style.transform = `translateY(${draggedYRef.current}px)`;
+    },
+    [isDragging, onTouchScreen]
+  );
+
   // When a column is dragged above/below its upper/lower limit respectively,
-  // lower/raise the column ti it's upper/lower limit.
-  const normalizeColumnPosition = () => {
+  // lower/raise the column until it's upper/lower limit.
+  // If a column is within it's bounds, snap it it it's closest grid position.
+  // A timer in a useEffect removes then transition animation.
+  const resetColumnAfterMouseUp = useCallback(() => {
     if (
       draggedYRef.current < COLUMN_UPPER_LIMIT ||
       draggedYRef.current > COLUMN_LOWER_LIMIT
@@ -161,20 +152,18 @@ function Column({
         draggedYRef.current < COLUMN_UPPER_LIMIT
           ? COLUMN_UPPER_LIMIT
           : COLUMN_LOWER_LIMIT;
-      columnRef.current.style.transform = `translate(0px, ${draggedYRef.current}px)`;
+      columnRef.current.style.transform = `translateY(${draggedYRef.current}px)`;
     } else {
-      var before = 0;
-      var after = BOX_HEIGHT;
+      var before = COLUMN_LOWER_LIMIT;
+      var after = COLUMN_LOWER_LIMIT - BOX_HEIGHT;
 
-      // While draggedYRef.current is NOT between the current before and after, go to next.
-      while (
-        !(before <= draggedYRef.current) ||
-        !(after > draggedYRef.current)
-      ) {
-        before += BOX_HEIGHT;
-        after += BOX_HEIGHT;
+      // Decrements before and after until draggedYRef is between those 2 values.
+      while (!isBetween(before, after, draggedYRef.current)) {
+        before -= BOX_HEIGHT;
+        after -= BOX_HEIGHT;
       }
 
+      // Column should snap to it's closest grid position.
       const distanceToBefore = Math.abs(draggedYRef.current - before);
       const distanceToAfter = Math.abs(draggedYRef.current - after);
 
@@ -186,26 +175,61 @@ function Column({
 
       // Animating column.
       setIsAnimated(true);
-      columnRef.current.style.transition = COLUMN_TRANSITION_ANIMATION;
-      columnRef.current.style.transform = `translate(0px, ${draggedYRef.current}px)`;
+      columnTranslatePositionRef.current = draggedYRef.current;
+      columnRef.current.style.transition = COLUMN_TRANSITION_ANIMATION; // Animation is removed via a useEffect.
+      columnRef.current.style.transform = `translateY(${draggedYRef.current}px)`;
     }
+  }, [COLUMN_LOWER_LIMIT, COLUMN_UPPER_LIMIT]);
+
+  // Checks is a value is between 2 other values.
+  // Returns a boolean.
+  const isBetween = (a, b, value) => {
+    // Both a and b are positive.
+    if (a >= 0 && b >= 0) {
+      if (value >= a && value < b) {
+        return true;
+      }
+    }
+    // BOth a and b are negative.
+    else if (a < 0 && b < 0) {
+      if (value <= a && value > b) {
+        return true;
+      }
+    }
+    // a is negative and b is positive.
+    else if (a < 0 && b >= 0) {
+      if (value >= a && value < b) {
+        return true;
+      }
+    }
+    // a is positive and b is negative.
+    else if (a >= 0 && b < 0) {
+      if (value <= a && value > b) {
+        return true;
+      }
+
+      return false;
+    }
+
+    return false;
   };
 
-  // Returns the index of the letter.
-  const calculateColumnPosition = () => {
-    const newPos = (draggedYRef.current / BOX_HEIGHT) * -1 + numOfBoxes - 1;
+  // Calculates the current word.
+  const calculateColumnPosition = useCallback(() => {
+    const newPos =
+      ((draggedYRef.current - COLUMN_LOWER_LIMIT) / BOX_HEIGHT) * -1;
+
     setColPosition(newPos);
 
-    var oldWord = viewedWord;
+    var oldWord = currentWord;
     oldWord = oldWord.split("");
 
     var newWord = oldWord;
-    newWord[index] = letters[newPos];
+    newWord[columnIndex] = letters[newPos];
     newWord = newWord.join("");
 
-    setViewedWord(newWord);
-    sendViewedWord(newWord);
-  };
+    setCurrentWord(newWord);
+  }, [COLUMN_LOWER_LIMIT, columnIndex, currentWord, letters, setCurrentWord]);
 
   // Sets opacity to 1 for both arrows.
   // Used with event listener.
@@ -234,6 +258,7 @@ function Column({
     return () => clearTimeout(timeoutId);
   }, [isAnimated]);
 
+  // Grows the letter stored in the crurentLetterRef.
   // Runs when a valid word is found.
   useEffect(() => {
     if (wordIsFound) {
@@ -251,43 +276,56 @@ function Column({
     }
   }, [wordIsFound, setWordIsFound]);
 
-  // After an animation is over (column slides to intended position),
-  // calculates which position the column is in.
-  useEffect(() => {
-    if (!isAnimated) {
-      calculateColumnPosition();
-    }
-  }, [isAnimated]);
-
   // On initial page load, places column on their starting position.
   useEffect(() => {
-    normalizeColumnPosition();
+    columnRef.current.style.transform = `translateY(${columnTranslatePositionRef.current}px)`;
   }, []);
 
+  // Adds event listeners for handling mouse up, mouse down, and dragging.
+  useEffect(() => {
+    const colRefVar = columnRef;
+
+    // Event listeners for mouse actions
+    colRefVar.current.addEventListener("mousedown", handleMouseDown);
+    window.addEventListener("mouseup", handleMouseUp);
+
+    colRefVar.current.addEventListener("touchstart", handleMouseDown);
+    window.addEventListener("touchend", handleMouseUp);
+
+    window.addEventListener("mousemove", handleDrag);
+    window.addEventListener("touchmove", handleDrag);
+
+    return () => {
+      colRefVar.current.removeEventListener("mousedown", handleMouseDown);
+      window.removeEventListener("mouseup", handleMouseUp);
+
+      colRefVar.current.removeEventListener("touchstart", handleMouseDown);
+      window.removeEventListener("touchend", handleMouseUp);
+
+      window.removeEventListener("mousemove", handleDrag);
+      window.removeEventListener("touchmove", handleDrag);
+    };
+  }, [handleMouseDown, handleDrag, handleMouseUp]);
+
   // On initial page load.
-  // Adds event listener to determine to window  to determine if user is on a touch device.
-  // Runs handleScreenSizeChange on load to set onTouchScreen state variable.
+  // Event listeners for arrows appearing around letter columns on hover.
   useEffect(() => {
     const colRefVar = columnRef;
 
     colRefVar.current.addEventListener("mouseover", addOpacity);
-    colRefVar.current.addEventListener("mouseover", addOpacity);
     colRefVar.current.addEventListener("touchstart", addOpacity);
 
-    colRefVar.current.addEventListener("mouseout", removeOpacity);
     colRefVar.current.addEventListener("mouseout", removeOpacity);
     colRefVar.current.addEventListener("touchend", removeOpacity);
 
     return () => {
       colRefVar.current.removeEventListener("mouseover", addOpacity);
-      colRefVar.current.removeEventListener("mouseover", addOpacity);
       colRefVar.current.removeEventListener("touchstart", addOpacity);
 
       colRefVar.current.removeEventListener("mouseout", removeOpacity);
-      colRefVar.current.removeEventListener("mouseout", removeOpacity);
       colRefVar.current.removeEventListener("touchend", removeOpacity);
     };
-  });
+  }, []);
 
   return (
     <div ref={columnRef} className="column" style={columnStyle}>
@@ -316,12 +354,13 @@ export default Column;
 
 Column.propTypes = {
   letters: PropTypes.array,
+  initialPosition: PropTypes.number,
   gameWon: PropTypes.bool,
-  viewedWord: PropTypes.string,
-  setViewedWord: PropTypes.func,
-  sendViewedWord: PropTypes.func,
-  index: PropTypes.number,
+  columnIndex: PropTypes.number,
   wordIsFound: PropTypes.bool,
   setWordIsFound: PropTypes.func,
   onTouchScreen: PropTypes.bool,
+  currentWord: PropTypes.string,
+  setCurrentWord: PropTypes.func,
+  longestColumn: PropTypes.number,
 };

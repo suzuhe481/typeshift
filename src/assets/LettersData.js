05212e422af6f5305580a22b500c58f11f3bd03d
Hector <baseballhector@gmail.com>
2024-04-03 19:10:08 -0400

    Data for letters

---
+++ src/assets/LettersData.js
@@ -0,0 +1,80 @@
+const LettersData = {
+  EASY: {
+    goalWords: ["CAT", "BIG", "BAG", "BIT", "BAT"],
+    ColumnData: [
+      {
+        letters: ["B", "C"],
+        initialPosition: 1,
+      },
+      {
+        letters: ["A", "I"],
+        initialPosition: 1,
+      },
+      {
+        letters: ["T", "G"],
+        initialPosition: 1,
+      },
+    ],
+  },
+  MEDIUM: {
+    goalWords: ["CAT", "BIG", "BAG", "BIT", "BAT"],
+    ColumnData: [
+      {
+        letters: ["B", "C"],
+        initialPosition: 1,
+      },
+      {
+        letters: ["A", "I"],
+        initialPosition: 1,
+      },
+      {
+        letters: ["T", "G"],
+        initialPosition: 1,
+      },
+    ],
+  },
+  HARD: {
+    goalWords: [
+      "POWER",
+      "PRINT",
+      "LARGE",
+      "TOWER",
+      "LOWER",
+      "DOWER",
+      "DRIER",
+      "TRINE",
+      "PAINT",
+      "PARED",
+      "LASER",
+      "LATER",
+      "LATTE",
+      "LOSER",
+      "TASED",
+      "TASER",
+    ],
+    ColumnData: [
+      {
+        letters: ["D", "T", "L", "P"],
+        initialPosition: 1,
+      },
+      {
+        letters: ["O", "R", "A"],
+        initialPosition: 1,
+      },
+      {
+        letters: ["T", "W", "S", "R", "I"],
+        initialPosition: 1,
+      },
+      {
+        letters: ["G", "E", "T", "N"],
+        initialPosition: 1,
+      },
+      {
+        letters: ["T", "D", "R", "E"],
+        initialPosition: 1,
+      },
+    ],
+  },
+};
+
+export default LettersData;

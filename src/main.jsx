09ef2d4e52ccc2a9c812a05eeea14dc3358acdc9
Hector <baseballhector@gmail.com>
2024-04-03 19:12:15 -0400

    Wrapping App in Context Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import GameOptionsProvider from "./Context/GameOptionsContext.jsx";
+
 import "./reset.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <GameOptionsProvider>
+      <App />
+    </GameOptionsProvider>
   </React.StrictMode>
 );

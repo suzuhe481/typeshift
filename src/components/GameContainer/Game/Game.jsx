63eb5e1afdc45a7df1682f9edf2d7a4b4a97fec6
Hector <baseballhector@gmail.com>
2024-04-09 18:41:40 -0400

    Removing unnecessary props passed

--- src/components/GameContainer/Game/Game.jsx
+++ src/components/GameContainer/Game/Game.jsx
@@ -41,25 +41,18 @@ function Game() {
   }, []);
 
   // Creates a column for each array in letters.
-  const columnItems = LettersData.map((col, index) => (
+  const columnItems = letters.map((col, index) => (
     <Column
       key={index}
       columnIndex={index}
       letters={col.letters}
       initialPosition={col.initialPosition}
-      gameWon={gameWon}
-      wordIsFound={wordIsFound}
-      setWordIsFound={setWordIsFound}
-      onTouchScreen={onTouchScreen}
-      currentWord={currentWord}
-      setCurrentWord={setCurrentWord}
-      longestColumn={longestColumn}
     />
   ));
 
   return (
     <div className="game">
-      <WinMessage gameWon={gameWon} />
+      <WinMessage />
       <div className="game__columns">{columnItems}</div>
       <div className="game__selection" style={gameSelectionStyle}></div>
     </div>
@@ -67,12 +60,3 @@ function Game() {
 }
 
 export default Game;
-
-Game.propTypes = {
-  LettersData: PropTypes.array,
-  gameWon: PropTypes.bool,
-  wordIsFound: PropTypes.bool,
-  setWordIsFound: PropTypes.func,
-  currentWord: PropTypes.string,
-  setCurrentWord: PropTypes.func,
-};

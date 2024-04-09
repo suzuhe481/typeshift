6571e4a5b14d956f3417f52aad462d1cc80fe20a
Hector <baseballhector@gmail.com>
2024-04-09 18:43:08 -0400

    Replaced gameWon prop with context variable

--- src/components/GameContainer/WinMessage/WinMessage.jsx
+++ src/components/GameContainer/WinMessage/WinMessage.jsx
@@ -1,8 +1,13 @@
+import { useContext } from "react";
 import PropTypes from "prop-types";
+
+import { GameOptionsContext } from "../../../Context/GameOptionsContext";
 
 import "./WinMessage.scss";
 
-function WinMessage({ gameWon }) {
+function WinMessage() {
+  const { gameWon } = useContext(GameOptionsContext);
+
   const message = (
     <div className={`game__message ${gameWon && "show"}`}>You Win!</div>
   );

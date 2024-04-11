8814e90d8d385d37759bea1a168c6ed7682672b6
Hector <baseballhector@gmail.com>
2024-04-11 19:30:54 -0400

    Added Play Again button

--- src/components/GameContainer/WinMessage/WinMessage.jsx
+++ src/components/GameContainer/WinMessage/WinMessage.jsx
@@ -6,10 +6,15 @@ import { GameOptionsContext } from "../../../Context/GameOptionsContext";
 import "./WinMessage.scss";
 
 function WinMessage() {
-  const { gameWon } = useContext(GameOptionsContext);
+  const { gameWon, resetGame } = useContext(GameOptionsContext);
 
   const message = (
-    <div className={`game__message ${gameWon && "show"}`}>You Win!</div>
+    <div className={`game__message ${gameWon ? "show" : "disabled"}`}>
+      <div>You Win!</div>
+      <button className="win-button" onClick={resetGame}>
+        Play again?
+      </button>
+    </div>
   );
 
   return <>{message}</>;

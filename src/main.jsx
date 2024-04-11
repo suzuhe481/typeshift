d1ad2bf77652c1eea96087568978c67aac3f8d84
Hector <baseballhector@gmail.com>
2024-04-11 17:51:17 -0400

    Implemented StylesProvider Context

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import GameOptionsProvider from "./Context/GameOptionsContext.jsx";
+import StylesProvider from "./Context/StylesContext.jsx";
 
 import "./reset.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GameOptionsProvider>
-      <App />
+      <StylesProvider>
+        <App />
+      </StylesProvider>
     </GameOptionsProvider>
   </React.StrictMode>
 );
